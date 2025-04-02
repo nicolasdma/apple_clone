9b10d477ebb24b70885e1bfae0162ea52ad5b946
Nicolás Demaria <nicolasdemaria123@gmail.com>
2025-04-02 14:51:43 -0500

    sentry 1

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import * as Sentry from "@sentry/react";
+import React from "react";
+
+Sentry.init({
+  dsn: "https://e324234781c4967dce326bb56144effa@o4509085338894336.ingest.us.sentry.io/4509085341057024",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
